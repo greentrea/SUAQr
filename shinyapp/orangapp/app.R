90e287bc4f826bb1c415fa9c3fe2e847e4f5a31d
greentrea <stefan.grafen@gmail.com>
2020-11-02 16:58:50 +0100

    finish shiny app

--- shinyapp/orangapp/app.R
+++ shinyapp/orangapp/app.R
@@ -32,6 +32,7 @@ library('remotes') # geom_convexhull function
 library('shiny')
 library('spdplyr')
 library('adehabitatHR')
+library('lubridate')
 library('ggplot2')
 
 #setwd("/Users/stefgr/Nextcloud/12_Semester/20200313_Masterthesis/R/shinyapp/orangapp")
@@ -59,8 +60,7 @@ SUAQ_pathnetwork <- st_transform(SUAQ_pathnetwork, 4326)
 # with mcp
 tmp_SUAQ_waypoints_morethan50gps_11all20<- SUAQ_waypoints_11_20_backup %>% 
   left_join(specs_orangutan_tot_num_of_gps[,c(1:3)],by=c("focal"="focal"))%>% 
-  filter(orangutan_tot_num_of_gps>=20)
-
+  filter(orangutan_tot_num_of_gps>=100)
 
 tmp_SUAQ_waypoints_morethan50gps_11all20.sp<- tmp_SUAQ_waypoints_morethan50gps_11all20 %>% dplyr::select(focal,E,N)
 coordinates(tmp_SUAQ_waypoints_morethan50gps_11all20.sp) <- c("E","N")
@@ -71,35 +71,46 @@ tmp_SUAQ_waypoints_morethan50gps_11all20.mcp<- mcp(tmp_SUAQ_waypoints_morethan50
 tmp_SUAQ_waypoints_morethan50gps_11all20.spgeo <- spTransform(tmp_SUAQ_waypoints_morethan50gps_11all20.sp, CRS("+proj=longlat"))
 tmp_SUAQ_waypoints_morethan50gps_11all20.mcpgeo <- spTransform(tmp_SUAQ_waypoints_morethan50gps_11all20.mcp, CRS("+proj=longlat"))
 
+tmp_SUAQ_waypoints_morethan50gps_11all20.geo <- data.frame(tmp_SUAQ_waypoints_morethan50gps_11all20.spgeo@coords, 
+                                                           id = tmp_SUAQ_waypoints_morethan50gps_11all20.spgeo@data$focal )
+
 mybasemap <- get_stamenmap(bbox = c(left = min(tmp_SUAQ_waypoints_morethan50gps_11all20.spgeo@coords[,1])-0.005, 
                                     bottom = min(tmp_SUAQ_waypoints_morethan50gps_11all20.spgeo@coords[,2])-0.005, 
                                     right = max(tmp_SUAQ_waypoints_morethan50gps_11all20.spgeo@coords[,1])+0.005, 
                                     top = max(tmp_SUAQ_waypoints_morethan50gps_11all20.spgeo@coords[,2])+0.005), 
                            zoom = 12)
-
 # API key: https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyABLRgWSFHCKuSyFF7QgZpa9ZgIqc9izZg
 
-tmp_SUAQ_waypoints_morethan50gps_11all20.geo <- data.frame(tmp_SUAQ_waypoints_morethan50gps_11all20.spgeo@coords, 
-                                                           id = tmp_SUAQ_waypoints_morethan50gps_11all20.spgeo@data$focal )
 
-
-
-
+tmp_SUAQ_waypoints_morethan50gps_11all20_unflmale <- tmp_SUAQ_waypoints_morethan50gps_11all20 %>% filter(SUAQ_followlog.ClassFocal %in% c("unfl.male"))
+tmp_SUAQ_waypoints_morethan50gps_11all20_others <- tmp_SUAQ_waypoints_morethan50gps_11all20 %>% filter(SUAQ_followlog.ClassFocal %in% c("ad.female","mother","infant","juvenile","unk"))
+tmp_SUAQ_waypoints_morethan50gps_11all20_flmale <- tmp_SUAQ_waypoints_morethan50gps_11all20 %>% filter(SUAQ_followlog.ClassFocal %in% c("fl.male"))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
+  
     # Application title
     titlePanel(h1("Minimum convex polygon for orangutans in SUAQ",
                   style='background-color:cadetblue;
-                     padding-left: 10px;padding-bottom:10px;padding-top:10px')),
+                     padding-left: 5px;padding-bottom:5px;padding-top:5px')),
     
     # Session Data
     
     # Sidebar with a slider input for number of bins
     sidebarLayout(position = "right",
           sidebarPanel(
+          style='background-color:#e5feff',
           br(),
+          sliderInput("daterange",
+                      "Dates: (minimum 14 days)",
+                      min = as.Date("2010-01-01","%Y-%m-%d"),
+                      max = as.Date("2020-12-01","%Y-%m-%d"),
+                      value=c(as.Date("2010-12-01"),as.Date("2016-12-01")),
+                      timeFormat="%Y-%m-%d"),
+          numericInput("mcppercentage","How many points should be considered for MCP? [0-100%]", value = 98),
+          # dateRangeInput("daterange", "Date range:",
+          #                start = min(tmp_SUAQ_waypoints_morethan50gps_11all20$manualDate, na.rm = TRUE),
+          #                end   = max(tmp_SUAQ_waypoints_morethan50gps_11all20$manualDate, na.rm = TRUE)),
           tags$style(".well {background-color:white;border-width:0px}"),
           tags$head(tags$script('$(document).on("shiny:connected", function(e) {
                             Shiny.onInputChange("innerWidth", window.innerWidth);
@@ -109,8 +120,12 @@ ui <- fluidPage(
                             });
                             ')),
             
-            checkboxGroupInput(inputId = "focalids",
-                               "Orangutan name",levels(as.factor(tmp_SUAQ_waypoints_morethan50gps_11all20$focal)),levels(as.factor(tmp_SUAQ_waypoints_morethan50gps_11all20$focal))[1],inline = T),
+            checkboxGroupInput(inputId = "focalids_unflmale",
+                               "Unflannged males",levels(as.factor(tmp_SUAQ_waypoints_morethan50gps_11all20_unflmale$focal)),levels(as.factor(tmp_SUAQ_waypoints_morethan50gps_11all20_unflmale$focal))[1],inline = T),
+            checkboxGroupInput(inputId = "focalids_flmale",
+                             "Flanged males",levels(as.factor(tmp_SUAQ_waypoints_morethan50gps_11all20_flmale$focal)),levels(as.factor(tmp_SUAQ_waypoints_morethan50gps_11all20_flmale$focal))[1],inline = T),
+            checkboxGroupInput(inputId = "focalids_female",
+                             "Others (Juvenile, ad. female, mother, infant, unknown)",levels(as.factor(tmp_SUAQ_waypoints_morethan50gps_11all20_others$focal)),levels(as.factor(tmp_SUAQ_waypoints_morethan50gps_11all20_others$focal))[1],inline = T),
           width = 3
         ),
         # Show a plot of the generated distribution
@@ -122,14 +137,41 @@ ui <- fluidPage(
     ),
 
     hr(),
-    print("Considered Data: 2010-2020 | MCP calculated with 98 percent of GPS points | Only focals with more than 20 GPS fixes are considered"),br(),
+    print("Considered Data: 2010-2020 | Only focals with more than 100 GPS fixes are considered"),br(),
     print("® Data is owned by the University of Zurich and is not allowed to use. Further information @stefan.grafen@gmail.com")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    output$mapPlot <- renderPlotly({
-        # tmp_SUAQ_selected_chulls <- SUAQ_waypoints_11_20_backup_sf %>%
+  output$mapPlot <- renderPlotly({
+    selected <- c(input$focalids_unflmale,input$focalids_flmale,input$focalids_female) 
+    # make sure end date later than start date
+    validate(
+      need(input$daterange[2] > input$daterange[1], "end date is earlier than start date"
+      )
+    )
+    
+    # make sure greater than 2 week difference
+    validate(
+      need(difftime(input$daterange[2], input$daterange[1], "days") > 14, "date range less the 14 days"
+      )
+    )
+    
+    # Prepare the data
+    tmp_SUAQ_waypoints_morethan50gps_11all20_server <- tmp_SUAQ_waypoints_morethan50gps_11all20 %>% filter(manualDate>as_date(input$daterange[1])) %>%filter(manualDate<as_date(input$daterange[2])) %>% filter(focal %in% selected)
+    tmp_SUAQ_waypoints_morethan50gps_11all20.sp<- tmp_SUAQ_waypoints_morethan50gps_11all20_server %>% dplyr::select(focal,E,N)
+    coordinates(tmp_SUAQ_waypoints_morethan50gps_11all20.sp) <- c("E","N")
+    proj4string(tmp_SUAQ_waypoints_morethan50gps_11all20.sp) <- CRS( "+proj=utm +zone=47 +datum=WGS84 +units=m +no_defs" )
+    
+    tmp_SUAQ_waypoints_morethan50gps_11all20.mcp<- mcp(tmp_SUAQ_waypoints_morethan50gps_11all20.sp,percent = input$mcppercentage)
+    
+    tmp_SUAQ_waypoints_morethan50gps_11all20.spgeo <- spTransform(tmp_SUAQ_waypoints_morethan50gps_11all20.sp, CRS("+proj=longlat"))
+    tmp_SUAQ_waypoints_morethan50gps_11all20.mcpgeo <- spTransform(tmp_SUAQ_waypoints_morethan50gps_11all20.mcp, CRS("+proj=longlat"))
+    
+    tmp_SUAQ_waypoints_morethan50gps_11all20.geo <- data.frame(tmp_SUAQ_waypoints_morethan50gps_11all20.spgeo@coords, 
+                                                               id = tmp_SUAQ_waypoints_morethan50gps_11all20.spgeo@data$focal )
+    
+      # tmp_SUAQ_selected_chulls <- SUAQ_waypoints_11_20_backup_sf %>%
         #     filter(focal %in% input$focalids) %>%
         #     left_join(specs_orangutan_tot_num_of_gps,by = c("focal" = "focal")) %>% 
         #     mutate(focal = paste(focal," [#GPS:", orangutan_tot_num_of_gps,"] [#Follows:",orangutan_num_of_follows,"]")) %>% 
@@ -142,19 +184,17 @@ server <- function(input, output) {
         #     geom_sf(data = SUAQ_pathnetwork,aes(fill = "path-network"))+
         #     labs(col="Focal")+
         #     theme(legend.title = element_blank(),legend.position = "bottom",plot.caption = element_text(hjust=0.5, size=rel(1.2)))
-      tmp_SUAQ_waypoints_morethan50gps_11all20.mcpgeo <- tmp_SUAQ_waypoints_morethan50gps_11all20.mcpgeo %>% filter(.$id %in% input$focalids)
-      tmp_SUAQ_waypoints_morethan50gps_11all20.geo <- tmp_SUAQ_waypoints_morethan50gps_11all20.geo %>% filter(.$id %in% input$focalids)
-      mcp_map_95 <- ggplot() + 
+   mcp_map_95 <- ggplot() + 
         geom_polygon(data = fortify(tmp_SUAQ_waypoints_morethan50gps_11all20.mcpgeo),  
                      # Polygon layer needs to be "fortified" to add geometry to the dataframe
                      aes(long, lat, colour = id),
                      alpha = 1,size=0.4,fill=NA) + # alpha sets the transparency
         geom_point(data = tmp_SUAQ_waypoints_morethan50gps_11all20.geo, 
                    aes(x = tmp_SUAQ_waypoints_morethan50gps_11all20.geo$E, y = tmp_SUAQ_waypoints_morethan50gps_11all20.geo$N, colour = 
-                         tmp_SUAQ_waypoints_morethan50gps_11all20.geo$id))  +
+                         tmp_SUAQ_waypoints_morethan50gps_11all20.geo$id),size=0.3,alpha=0.6)  +
         theme(legend.position = c(0.15, 0.80),plot.title =element_blank()) +
         labs(x = "Longitude", y = "Latitude")+
-        geom_sf(data = SUAQ_pathnetwork, inherit.aes = FALSE)
+        geom_sf(data = SUAQ_pathnetwork, inherit.aes = FALSE,size=0.1)
       ggplotly(mcp_map_95)
     })
 }
